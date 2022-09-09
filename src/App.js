c55c36969a70e6dff3f9793c3257338924083916
Guilherme Mir <guilherme.mir@gmail.com>
2022-09-09 01:09:07 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
+import MovieCard from './MovieCard';
+import SearchIcon from './search.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = 'http://www.omdbapi.com?apikey=38867bba'
+
+const App = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+      searchCurrentTerm();
+    }, []);
+
+    const searchCurrentTerm = () => {
+      searchMovies(searchTerm);
+    }
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+
+    return (
+      <div className="app">
+        <h1>MirFlix</h1>
+
+        <div className="search">
+          <input
+            value={searchTerm}
+            onChange={(e) => {
+              setSearchTerm(e.target.value)
+             } }
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                setSearchTerm(e.target.value)
+                searchCurrentTerm();
+              }
+             } }
+            placeholder="Busque por um filme..."
+          />
+          <img
+            src={SearchIcon}
+            alt="Buscar"
+            onClick={() => searchCurrentTerm()}
+          />
+        </div>
+
+        {
+          movies?.length > 0 ? (
+            <div className="container">
+              {
+                movies.map(movie => (
+                  <MovieCard movie={movie} />
+                ))
+              }
+            </div>
+          ) : (
+            searchTerm === '' ? (
+              <div className="empty">
+                <h2>Procure por um filme :)</h2>
+              </div>
+            ) : (
+              <div className="empty">
+                <h2>Nenhum filme encontrado chamado <em>{searchTerm}</em>.</h2>
+              </div>
+            )
+          )
+        }
+      </div>
+    );
 }
 
 export default App;
